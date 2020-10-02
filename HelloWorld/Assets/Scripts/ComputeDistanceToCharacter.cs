e50e5064179a502566cfdbca90f7ca1dd2d092d0
SergiuDev <sergiu.nistor@info.uaic.ro>
2020-10-02 12:13:49 +0300

    Added mock animations and triggers

---
+++ HelloWorld/Assets/Scripts/ComputeDistanceToCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputeDistanceToCharacter : MonoBehaviour
+{
+    public GameObject PlayerObject;
+
+    private Animator cactusAnimator;
+    private float distanceFromPlayer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        cactusAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateCactusAnimatorParams();
+    }
+
+    private void UpdateCactusAnimatorParams() {
+        distanceFromPlayer = Vector3.Distance(transform.position, PlayerObject.transform.position);
+        cactusAnimator.SetFloat("DistanceFromPlayer", distanceFromPlayer);
+    }
+}
